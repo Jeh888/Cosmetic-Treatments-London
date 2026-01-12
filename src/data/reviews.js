a9e673f6bfc0ebcf778e107ab1596529673bc729
Jeh888 <jerwinmanongsong2@gmail.com>
2026-01-13 01:20:50 +0800

    Update reviews.js

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -1,112 +1,112 @@
-// reviews.js - Customer testimonials
+// reviews.js - Customer testimonials for aesthetic treatments
 
 export const reviews = [
   {
     id: 1,
     name: 'Sarah M.',
     location: 'Clapham',
-    treatment: 'Teeth Whitening',
+    treatment: 'Botox',
     rating: 5,
-    text: 'Found an amazing clinic through this site. The whole process was so easy - filled in the form and had three quotes within 24 hours. My teeth are now 6 shades whiter!',
+    text: 'Found an amazing practitioner through this site. The whole process was so easy - filled in the form and had three quotes within 24 hours. My forehead lines are gone and I look so refreshed!',
     date: '2024-11-15'
   },
   {
     id: 2,
     name: 'James T.',
     location: 'Canary Wharf',
-    treatment: 'Invisalign',
+    treatment: 'Jawline Fillers',
     rating: 5,
-    text: 'Being able to compare Invisalign providers saved me over £1,000. The clinic I chose was professional, affordable, and my teeth are perfectly straight now.',
+    text: 'Being able to compare providers saved me over £400. The clinic I chose was professional, and my jawline looks so much more defined now. Really pleased with the results.',
     date: '2024-11-10'
   },
   {
     id: 3,
     name: 'Priya K.',
     location: 'Wembley',
-    treatment: 'Botox',
+    treatment: 'Lip Fillers',
     rating: 5,
-    text: 'I was nervous about getting Botox but the provider I found through this service made me feel completely at ease. Natural results and half the price of other quotes.',
+    text: 'I was nervous about getting lip fillers but the provider I found through this service made me feel completely at ease. Natural results and half the price of other quotes.',
     date: '2024-11-08'
   },
   {
     id: 4,
     name: 'Michael R.',
     location: 'Brixton',
-    treatment: 'Veneers',
+    treatment: 'Cheek Fillers',
     rating: 5,
-    text: 'Always wanted a Hollywood smile but thought it was out of reach. Compared quotes from five clinics and found one with finance options. Best decision ever!',
+    text: 'Wanted to restore some volume to my face. Compared quotes from five clinics and found one with payment plans. The lift is incredible - took years off my face!',
     date: '2024-10-28'
   },
   {
     id: 5,
     name: 'Emma L.',
     location: 'Putney',
-    treatment: 'Lip Fillers',
+    treatment: 'Dermal Fillers',
     rating: 5,
-    text: 'Used this site to find a reputable lip filler provider after a bad experience elsewhere. The practitioner was highly qualified and my lips look amazing - subtle but fuller.',
+    text: 'Used this site to find a reputable filler provider after a bad experience elsewhere. The practitioner was highly qualified and my nasolabial folds are so much softer now.',
     date: '2024-10-25'
   },
   {
     id: 6,
     name: 'David H.',
     location: 'Stratford',
-    treatment: 'Dental Crowns',
+    treatment: 'Chemical Peel',
     rating: 5,
-    text: 'Needed crowns on two teeth and the quotes I received varied massively. This site helped me find a great dentist who did excellent work at a fair price.',
+    text: 'Had issues with pigmentation for years. The quotes I received varied massively. This site helped me find a great clinic who did a course of peels - my skin has never looked better.',
     date: '2024-10-20'
   },
   {
     id: 7,
     name: 'Sophie W.',
     location: 'Bromley',
-    treatment: 'Smile Makeover',
+    treatment: 'Profhilo',
     rating: 5,
-    text: 'My complete smile makeover journey started here. The clinic I chose offered a full consultation, digital preview, and flexible payments. Life-changing results!',
+    text: 'Everyone kept asking if I\'d been on holiday - that\'s how good my skin looks after Profhilo! The clinic I found through this service was brilliant and very fairly priced.',
     date: '2024-10-15'
   },
   {
     id: 8,
     name: 'Amir N.',
     location: 'Finchley',
-    treatment: 'Dermal Fillers',
+    treatment: 'Tear Trough Fillers',
     rating: 5,
-    text: 'Wanted to reduce my nasolabial folds and wasn\'t sure where to start. Got connected with an excellent aesthetic practitioner who delivered exactly what I wanted.',
+    text: 'Wanted to reduce my under-eye hollows and wasn\'t sure where to start. Got connected with an excellent aesthetic practitioner who delivered exactly what I wanted.',
     date: '2024-10-12'
   },
   {
     id: 9,
     name: 'Rebecca T.',
     location: 'Lewisham',
-    treatment: 'Dental Bonding',
+    treatment: 'Microneedling',
     rating: 5,
-    text: 'Had a chipped front tooth that was really affecting my confidence. Found a dentist who fixed it in one visit with bonding - looks completely natural!',
+    text: 'Had acne scarring that really affected my confidence. Found a clinic who did a course of microneedling - my skin texture has improved dramatically!',
     date: '2024-10-08'
   },
   {
     id: 10,
     name: 'Chris P.',
     location: 'Uxbridge',
-    treatment: 'Teeth Whitening',
+    treatment: 'Fat Dissolving',
     rating: 5,
-    text: 'Quick and easy to get quotes. The clinic was convenient for me and did an incredible job. Would definitely recommend using this comparison service.',
+    text: 'Quick and easy to get quotes for my double chin treatment. The clinic was convenient for me and did an incredible job. Highly recommend this comparison service.',
     date: '2024-10-05'
   },
   {
     id: 11,
     name: 'Nina S.',
     location: 'Battersea',
-    treatment: 'Botox',
+    treatment: 'Laser Hair Removal',
     rating: 5,
-    text: 'As a first-timer, I really appreciated being able to compare practitioners. Found someone highly experienced who took time to explain everything.',
+    text: 'As a first-timer, I really appreciated being able to compare practitioners. Found someone highly experienced with great equipment. No more shaving for me!',
     date: '2024-10-01'
   },
   {
     id: 12,
     name: 'Tom B.',
     location: 'Croydon',
-    treatment: 'Invisalign',
+    treatment: 'PRP Facial',
     rating: 5,
-    text: 'The comparison saved me hours of research. All the providers who contacted me were legitimate and professional. Now 6 months into treatment with great results!',
+    text: 'The comparison saved me hours of research. All the providers who contacted me were legitimate and professional. My skin looks younger and more radiant!',
     date: '2024-09-28'
   }
 ];
