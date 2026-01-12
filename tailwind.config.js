7b24532d3a205dac707a2c4ad651e6dfb19283eb
Jeh888 <jerwinmanongsong2@gmail.com>
2026-01-12 19:32:17 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,18 +9,6 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          50: '#fdf4ff',
-          100: '#fae8ff',
-          200: '#f5d0fe',
-          300: '#f0abfc',
-          400: '#e879f9',
-          500: '#d946ef',
-          600: '#c026d3',
-          700: '#a21caf',
-          800: '#86198f',
-          900: '#701a75',
-        },
-        secondary: {
           50: '#f0fdfa',
           100: '#ccfbf1',
           200: '#99f6e4',
